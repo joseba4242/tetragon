9cd78ade539da7da68615ab9efb873acf4081287
Jiri Olsa <jolsa@kernel.org>
2024-06-16 19:59:04 +0000

    tetragon: Add PinPath to Map object
    Adding PinPath to Map object to carry path relative to the sysfs
    bpf root tetragon tree.
    
    At the moment we have map's Name as a real (bpf object) name and
    PinName when we need to pin map under different name.
    
    The PinName will be removed once we move to new hierarchy structure,
    but we still need to keep the relative pinned path of the map.
    
    Signed-off-by: Jiri Olsa <jolsa@kernel.org>

--- pkg/sensors/program/loader.go
+++ pkg/sensors/program/loader.go
@@ -869,7 +869,7 @@ func doLoadProgram(
 		var err error
 		var mapPath string
 		if pm, ok := load.PinMap[name]; ok {
-			mapPath = filepath.Join(bpfDir, pm.PinName)
+			mapPath = filepath.Join(bpfDir, pm.PinPath)
 		} else {
 			mapPath = filepath.Join(bpfDir, name)
 		}
